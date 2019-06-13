50f5c34007e675a3a6a3260f4dd1379a1cd7fff7
Paula Thomas <paula9t9@gmail.com>
2019-06-13 10:10:46 -0700

    Displays data from database

--- src/main/java/com/fedyr/code401/firstWebApp/AlbumController.java
+++ src/main/java/com/fedyr/code401/firstWebApp/AlbumController.java
@@ -14,17 +14,8 @@ public class AlbumController {
     // Album names pulled from Album name generator: https://www.fantasynamegenerators.com/album-names.php
     @GetMapping("/albums")
     public String getAlbums(Model m){
-        String[] albumTitles = new String[]{"Crime of the century\n" +
-                "Grains of sand\n" +
-                "Rocket surgery\n" +
-                "Glass shoe\n" +
-                "Game on\n" +
-                "Elephant in the room\n" +
-                "Knowledge bomb\n" +
-                "Nine yards\n" +
-                "Dinner for one\n" +
-                "Crowd control"};
-        m.addAttribute("albumTitles", albumTitles);
+        Iterable<Album> albums = albumRepository.findAll();
+        m.addAttribute("albums", albums);
         return "albums";
     }
 }
