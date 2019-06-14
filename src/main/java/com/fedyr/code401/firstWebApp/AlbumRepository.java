61edfbf79e88667906badcefeae614a5ed3a7e72
Paula Thomas <paula9t9@gmail.com>
2019-06-14 12:23:15 -0700

    Album details page displays

--- src/main/java/com/fedyr/code401/firstWebApp/AlbumRepository.java
+++ src/main/java/com/fedyr/code401/firstWebApp/AlbumRepository.java
@@ -3,5 +3,8 @@ package com.fedyr.code401.firstWebApp;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface AlbumRepository extends CrudRepository<Album, Long> {
+
 }
